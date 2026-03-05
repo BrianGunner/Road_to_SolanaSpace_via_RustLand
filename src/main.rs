a20fdcb791126ab124a4e52e976e0592db372007
BrianGunner <bhargav@avalialearning.com>
2026-03-05 15:01:06 +0530

    Enums with BankAccount

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,42 @@
+#[derive(Debug)]
+enum AccountStatus{
+    Active,
+    Inactive,
+    Frozen,
+}
+
 struct BankAccount{
     id:u32,
     owner:String,
-    balance:i64
+    balance:i64,
+    status:AccountStatus,
 }
 
 impl BankAccount{
-    fn new(id:u32,owner:String,balance:i64)->BankAccount{
-        BankAccount { id, owner, balance }
-    }
-    fn print_acc_details(&self){
-        println!("Id:{},Balance:{},Name:{}",self.id,self.balance,self.owner);
-    }
-    fn deposit(&mut self,amount:i64){
-        self.balance += amount
-    }
-    fn withdraw(&mut self,amount:i64){
-        if self.balance>amount{
-            self.balance-=amount
-        }
-        else{
-            println!("Insufficient funds")
-        }
-    }
-    fn is_rich(&self)->bool{
-        let mut is_rich = false;
-        if self.balance>5000{
-            is_rich=true
-        }
-        return is_rich;
-
+    fn new(id:u32,owner:String)->BankAccount{
+        BankAccount { id:id, owner:owner, balance:0, status:AccountStatus::Active}
     }
 
-    fn ten_k_account(name:String)->BankAccount{
-        BankAccount { id: 0, owner: name, balance: 10000 }
+    fn print_details(&self){
+        println!("Name: {}, id: {}, balance: {}, status: {:?}",self.owner,self.id,self.balance,self.status);
+    }
+
+    fn is_active(&self)->bool{
+        match self.status{
+            AccountStatus::Active=>true,
+            _=>false,
+        }
+    }
+
+    fn freeze(&mut self){
+        self.status=AccountStatus::Frozen
     }
 }
 
-
 fn main(){
-    let mut acc = BankAccount::new(2, "Stanley's".to_string(), 25000);
-    acc.print_acc_details();
-    acc.deposit(99);
-    acc.print_acc_details();
-    acc.withdraw(10000);
-    acc.deposit(10000);
-    acc.print_acc_details();
-    println!("The account is rich? - {}",acc.is_rich());
-    let mut acc2 = BankAccount::ten_k_account("Potti".to_string());
-    acc2.print_acc_details();
-
-    
+    let mut acc1 = BankAccount::new(1, "Bhargav".to_string());
+    acc1.print_details();
+    println!("Account status: {}",acc1.is_active());
+    acc1.freeze();
+    println!("Account status: {}",acc1.is_active());
 }
