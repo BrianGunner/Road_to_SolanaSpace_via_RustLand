43da407caaa7ae6cec19ff2680f6453ef7a3bc14
BrianGunner <bhargav@avalialearning.com>
2026-03-09 17:56:09 +0530

    Enums + Result: AccountStatus with freeze and check_active_status

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
 
-
 struct BankAccount{
     id:u32,
     balance:i64,
+    status:AccountStatus,
+    
+ }
+
+#[derive(Copy, Clone)]
+#[derive(Debug)]
+ enum AccountStatus {
+    Active,
+    Inactive,
+    Frozen,
+     
  }
 
  impl BankAccount {
     fn create_new_account(acc:&mut Vec<BankAccount>,id:u32){
-        acc.push(BankAccount { id:id, balance: 0 });
+        acc.push(BankAccount { id:id, balance: 0,status:AccountStatus::Active });
     }
 
     fn add_balance(accounts:&mut Vec<BankAccount>,amount:i64,id:u32)->Result<i64,String>{
@@ -29,6 +39,32 @@ struct BankAccount{
             
         }
         return Err("Could not find account id".to_string());
+    }
+
+    fn is_active(&self)->bool{
+       match self.status{
+        AccountStatus::Active=>true,
+        _=>false,
+       }
+    }
+
+    fn check_active_status(accounts:&Vec<BankAccount>,id:u32)->Result<AccountStatus,String>{
+        for acc in accounts.iter(){
+            if acc.id==id{
+                return Ok(acc.status);
+            }
+        }
+        Err("Could not find id".to_string())
+    }
+
+    fn freeze(accounts:&mut Vec<BankAccount>,id:u32)->Result<(),String>{
+        for acc in accounts.iter_mut(){
+            if acc.id==id{
+                acc.status=AccountStatus::Frozen;
+                return Ok(());
+            }
+        }
+        Err("Could not find id".to_string())
     }
      
  }
@@ -60,6 +96,21 @@ struct BankAccount{
     for acc in accounts.iter(){
         println!("id:{}-balance:{}",acc.id,acc.balance)
     }
+
+    BankAccount::freeze(&mut accounts, 3);
+
+    for acc in accounts.iter(){
+        if acc.id==3{
+            let status = acc.is_active();
+            println!("acc.id: {} is {}",acc.id,status);
+        }
+    }
+
+    match BankAccount::check_active_status(&accounts, 3){
+        Ok(status)=>println!("{:?}",status),
+        Err(msg)=>println!("{}",msg)
+    }
+    
 
 
 
