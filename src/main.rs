e95bef1bf01e4b97d49baa2ce724b16361b5845a
BrianGunner <bhargav@avalialearning.com>
2026-03-24 13:27:20 +0530

    Lottery Atomic transfers

--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,138 @@
-#[derive(Debug)]
 struct Player{
-    id:u32,
-    name:String,
-    balance:u64,
+    id: u32,
+    name: String,
+    balance: u64,
+}
+#[derive(Debug)]
+enum LotteryState{
+    Open, 
+    Drawing, 
+    Closed
 }
 
-impl Player{
-    fn add_new_player(id:u32,name:String)->Player{
-        Player { id:id, name:name, balance: 0 }
-    }
-
-    fn add_balance(&mut self,amount:u64){
-        self.balance += amount
-    }
-}
-
-enum LotteryState {
-    Open,
-    Drawing,
-    Close,
-}
-
-struct Lottery {
+struct Lottery{
     state:LotteryState,
     pool:u64,
-    players:Vec<Player>
-    
+    player_ids:Vec<u32>,
+    ticket_price:u64,
 }
 
 impl Lottery{
-    fn open_new_lottery()->Lottery{
-        Lottery {state:LotteryState::Open, pool:0,players:Vec::new()}
+    fn new()->Lottery{
+        Lottery {state: LotteryState::Open, pool: 0, player_ids: Vec::new(),ticket_price:100}
     }
 
-    fn add_player(&mut self, mut players:Player)->Result<(),String>{
-        if players.balance<50{
-            return Err("Not enough balance".to_string());
+    fn add_players_lottery(&mut self,players:&mut Vec<Player>,id:u32)->Result<(),String>{
+
+        for lottery_id in self.player_ids.iter(){
+            if *lottery_id==id{
+                return Err("Player id already exists in lottery".to_string());
+            }
         }
-        players.balance-=50;
-        self.pool+=50;
-        self.players.push(players);
-        return Ok(());
+
+        for player in players.iter_mut(){
+            if player.id == id{
+                if player.balance<self.ticket_price{
+                return Err("Insufficient Balance".to_string());
+            }
+                player.balance -= self.ticket_price;
+                self.pool += self.ticket_price;
+                self.player_ids.push(id);
+                return Ok(());
+            }
+            
+        }
+        
+            
+        
+
+        
+
+        Err("Player id does not exist".to_string())
+
     }
 
-    fn lottery_state(&self){
-        println!("Total number of players joined: {}",self.players.len());
-        for player in self.players.iter(){
-            println!("Player: {}",player.balance);
+    fn print_lottery_state(&self){
+        println!("Lottery State: {:?}",self.state);
+        println!("Lottery Pool Amount: {}",self.pool);
+        println!("Player ids joined Lottery: ");
+        for id in self.player_ids.iter(){
+            print!("{},",id);
         }
-        println!("Total lottery pool: {}",self.pool);
     }
 }
 
 
+impl Player {
+
+    fn new(players:&mut Vec<Player>,id:u32,name:String){
+        players.push(Player {id:id , name:name, balance:0});
+    }
+
+    fn print_players(players:&Vec<Player>){
+        for player in players.iter(){
+            println!("Name: {}, Balance: {}, id: {}",player.name,player.balance,player.id);
+        }
+    }
+
+    fn fund_players(players:&mut Vec<Player>,amount:u64,id:u32)->Result<(),String>{
+        
+        
+        for player in players.iter_mut(){
+            if player.id==id{
+                player.balance += amount;
+                return Ok(());
+            }  
+        }
+
+        Err("Id not found".to_string())
+    }
+    
+}
 
 fn main(){
 
-    let mut player_1 = Player::add_new_player(1,"Bhargav".to_string());
-    let mut player_2 = Player::add_new_player(1, "Potti".to_string());
-    player_1.add_balance(100);
-    player_2.add_balance(100);
-    let mut lottery_1 = Lottery::open_new_lottery();
-    match lottery_1.add_player(player_1){
-        Ok(())=>println!("Player added"),
+    let mut players:Vec<Player> = Vec::new();
+    Player::new(&mut players, 1, "Bhargav".to_string());
+    Player::new(&mut players, 2, "Potti".to_string());
+    match Player::fund_players(&mut players, 100, 2){
+        Ok(())=>println!("Balance added"),
         Err(msg)=>println!("{}",msg),
     }
-    match lottery_1.add_player(player_2){
-        Ok(())=>print!("Player added"),
-        Err(msg)=>println!("{}",msg),
-    }
-    lottery_1.lottery_state();
+    Player::print_players(&players);
 
-    
+    let mut lottery_1 = Lottery::new();
+
+    match lottery_1.add_players_lottery(&mut players, 2){
+        Ok(())=>println!("Valid player id found and added to lottery"),
+        Err(msg)=>println!("{}",msg),
+    }
+    match lottery_1.add_players_lottery(&mut players, 2){
+        Ok(())=>println!("Valid player id found and added to lottery"),
+        Err(msg)=>println!("{}",msg),
+    }
+    match lottery_1.add_players_lottery(&mut players, 1){
+        Ok(())=>println!("Valid player id found and added to lottery"),
+        Err(msg)=>println!("{}",msg),
+    }
+
+    match Player::fund_players(&mut players, 100, 1){
+        Ok(())=>println!("Balance Added"),
+        Err(msg)=>println!("{}",msg),
+    }
+
+     match lottery_1.add_players_lottery(&mut players, 1){
+        Ok(())=>println!("Valid player id found and added to lottery"),
+        Err(msg)=>println!("{}",msg),
+    }
+
+
   
-   
-    
+
+    lottery_1.print_lottery_state();
+
+
 
     
+
 }
