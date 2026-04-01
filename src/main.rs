530ae4bad56a1762a6e5fe636d93aaa760c51f5e
BrianGunner <bhargav@avalialearning.com>
2026-04-01 18:00:31 +0530

    Simple ? that avoids match

--- src/main.rs
+++ src/main.rs
@@ -11,48 +11,23 @@ fn add_two_numbers(a:i32,b:i32)->Result<i32,String>{
 }
 
 fn add_and_double(a:i32,b:i32)->Result<i32,String>{
-    match add_two_numbers(a, b){
-        Ok(value)=>Ok(value*2),
-        Err(msg)=>return Err(msg),
-    }
+    let result = add_two_numbers(a, b)?;
+    Ok(result*2)
 }
 
-fn add_and_double_and_add(a:i32,b:i32)->i32{
-    //take a+b, then take double and then add both again
-    let mut add = 0;
-    let mut double = 0;
-    match add_two_numbers(a, b){
-        Ok(value)=>add=value,
-        Err(msg)=>(),
-    }
-    match add_and_double(a, b){
-        Ok(value)=>double=value,
-        Err(msg)=>()
-    }
-    return add+double;
-
+fn add_and_double_and_add(a:i32,b:i32)->Result<i32,String>{
+    let result_add = add_two_numbers(a, b)?;
+    let result_double = add_and_double(a, b)?;
+    Ok(result_add+result_double)
 }
 
 
 
-fn main(){
+fn main()->Result<(),String>{
+   let value =  add_two_numbers(10,20)?;
+   println!("Sum of two numbers is: {}",value);
+   Ok(())
+   
 
-    let a=-10;
-    let b = 20;
-
-    let result = add_two_numbers(a,b);
-    match result{
-        Ok(value)=>println!("The result is : {}",value),
-        Err(msg)=>println!("{}",msg)
-    }
-
-    let result_2 = add_and_double(a, b);
-    match result_2 {
-        Ok(value)=>println!("Doubled value: {}",value),
-        Err(msg)=>println!("{}",msg),
-    }
-
-    let add_plus_double = add_and_double_and_add(a, b);
-    println!("Add and double and add is : {}",add_plus_double);
 
 }
