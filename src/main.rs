d8feb014b74c0c427c1f0152d0b69f34ea7b1189
BrianGunner <bhargav@avalialearning.com>
2026-04-19 11:40:57 +0530

    Learning ?

--- src/main.rs
+++ src/main.rs
@@ -4,53 +4,29 @@ struct Account{
     balance:i32,
 }
 
-fn find_account_index(accounts:&mut Vec<Account>,from_id:u32,to_id:u32)->Result<(usize,usize),String>{
-    let mut from_id_index = None;
-    let mut to_id_index = None;
-
+fn index_find(accounts:& Vec<Account>,id:u32)->Result<usize,String>{
     for (index,acc) in accounts.iter().enumerate(){
-        if from_id == acc.id{
-            from_id_index = Some(index);
-        }
-        
-    }
-
-    for (index,acc) in accounts.iter().enumerate(){
-        if to_id == acc.id{
-            to_id_index = Some(index);
+        if acc.id == id{
+            return Ok(Some(index).ok_or("Id not found,None,ok_or".to_string())?);
         }
     }
-    let mut from_id_index_final = from_id_index.ok_or("Could not find from_id".to_string())?;
-    
-    let mut to_id_index_final = to_id_index.ok_or("Could not find to_id".to_string())?;
-    
-    return Ok((from_id_index_final,to_id_index_final));
-
+    Err("Id not found,Err".to_string())
 }
 
 fn main(){
-    let mut accounts = vec![
-        Account{id:1,balance:0},
-        Account{id:2,balance:99},
-        Account{id:3,balance:25000},
-    ];
+    let accounts = vec![
+        Account{id:1,balance:25000},
+        Account{id:2,balance:0},
+        Account{id:3,balance:3434},
+        ];
 
-    let found = find_account_index(&mut accounts, 3, 1);
-    let mut from_index= 0 as usize;
-    let mut to_index = 0 as usize;
+    let found = index_find(&accounts, 8);
+
     match found {
-        Ok(value)=>{(from_index,to_index) = value;},
+        Ok(value)=>println!("{}",value),
         Err(msg)=>println!("{}",msg),
         
     }
-    let amount = 500;
-    let (left,right) = accounts.split_at_mut(2);
-    println!("left = {:?},right = {:?}",left,right);
-    {
-        right[0].balance +=amount;
-        left[0].balance += amount;
-    }
 
-    
-
+     
 }
