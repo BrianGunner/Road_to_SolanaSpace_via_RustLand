94fd7a2892842e64e71f82cf807e800cc5df08f6
BrianGunner <bhargav@avalialearning.com>
2026-04-28 15:08:12 +0530

    ? flow

--- src/main.rs
+++ src/main.rs
@@ -3,45 +3,32 @@ struct Account{
     balance:i32,
 }
 
-impl Account{
-    fn find_index(accounts:&Vec<Account>,id:u32)->Option<usize>{
-        for (index,acc) in accounts.iter().enumerate(){
-            if acc.id == id{
-                return Some(index);
-            }
+fn first_1000(accounts:&Vec<Account>)->Result<i32,String>{
+    for acc in accounts.iter(){
+        if acc.balance>1000{
+            return Ok(acc.balance);
         }
-        None
     }
-
-    fn find_index_ft(accounts:&Vec<Account>,from_id:u32,to_id:u32)->Result<(usize,usize),String>{
-        let from_id_index = Account::find_index(accounts, from_id).ok_or("No from_id".to_string())?;
-        let to_id_index = Account::find_index(accounts, to_id).ok_or("No to id".to_string())?;
-        return Ok((from_id_index,to_id_index));
-
-    }
+    Err("Could not find".to_string())
 }
 
+fn add_100(accounts:&Vec<Account>)->Result<i32,String>{
+    let result = first_1000(accounts)?+100;
+    Ok(result)
+}
 
-
-
-
-fn main(){
-    let mut accounts = vec![
-        Account{id:1,balance:9999},
+fn main()->Result<(),String>{
+    let accounts = vec![
+        Account{id:1,balance:999},
         Account{id:2,balance:0},
-        Account{id:3,balance:8878},
-        Account{id:4,balance:0},
+        Account{id:3,balance:887},
+        Account{id:4,balance:111},
     ];
-
-    let fun_test = Account::find_index_ft(&accounts, 2, 3);
-    let mut left = 0 as usize;
-    let mut right = 0 as usize;
-    match fun_test{
-        Ok(value)=>{
-            (left,right) = value;
-        },
-        Err(msg)=>println!("{}",msg),
-    }
+    
+let result_fetch = add_100(&accounts)?;
+println!("{}",result_fetch);
+Ok(())
+    
     
 
 }
