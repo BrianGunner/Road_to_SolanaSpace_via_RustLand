07339fb45c4bbbf30f8cada8f0cf11368ec57c48
BrianGunner <bhargav@avalialearning.com>
2026-04-29 12:26:38 +0530

    My first atomic transfer

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,72 @@
+#[derive(Debug)]
 struct Account{
     id:u32,
     balance:i32,
 }
 
-fn find_1000(accounts:&Vec<Account>)->Result<i32,String>{
-    for acc in accounts.iter(){
-        if acc.balance>=1000{
-            return Ok(acc.balance);
+fn find_index(accounts:&Vec<Account>,id:u32)->Option<usize>{
+    for (index,acc) in accounts.iter().enumerate(){
+        if acc.id == id{
+            return Some(index)
         }
     }
-    Err("Could not find acc above 1000".to_string())
+    None
 }
 
-fn find_halve(accounts:&Vec<Account>)->Result<i32,String>{
-    let result_halve = find_1000(accounts)?;
-    Ok(result_halve/2)
-}
-
-fn find_halve_even(accounts:&Vec<Account>)->Result<i32,String>{
-    let result_halve_even = find_halve(accounts)?;
-    if result_halve_even%2==0{
-        Ok(result_halve_even)
+fn transfer(accounts:&mut Vec<Account>,from_id:u32,to_id:u32,amount:i32)->Result<(),String>{
+    let from_id_index = find_index(accounts, from_id).ok_or("from_id not found".to_string())?;
+    let to_id_index = find_index(accounts, to_id).ok_or("to_id not found".to_string())?;
+    if from_id_index==to_id_index{
+        return Err("from id cannot be same as to id".to_string());
     }
-    else{
-        Err("Result not even".to_string())
+    if accounts[from_id_index].balance<amount{
+        return Err("Insufficient balance".to_string());
     }
-}
+    if from_id_index<to_id_index{
+       let (left,right) = accounts.split_at_mut(to_id_index);
+        left[from_id_index].balance-=amount;
+        right[0].balance+=amount;
+    }
+    else {
+        let(left,right)=accounts.split_at_mut(from_id_index);
+        right[0].balance-=amount;
+        left[to_id_index].balance+=amount;
+    }
 
-fn main()->Result<(),String>{
-    let accounts = vec![
-        Account{id:1,balance:1000},
-        Account{id:2,balance:0},
-        Account{id:3,balance:887},
-        Account{id:4,balance:111},
-    ];
-
-    let final_result = find_halve_even(&accounts)?;
-    println!("{:?}",final_result);
     Ok(())
+}
+
+fn print_state(accounts:&Vec<Account>){
+    println!("🤑++++++++++++++++🤑");
+    for acc in accounts.iter(){
+        
+        println!("Account id: {} - Account balance: {}",acc.id,acc.balance);
     
- 
+    }
+}
+
+
+
+
+fn main(){
+    let mut accounts = vec![
+        Account{id:1,balance:1000},
+        Account{id:2,balance:110},
+        Account{id:3,balance:0},
+        Account{id:4,balance:111},
+        Account{id:5,balance:98898},
+    ];
+    
+    print_state(&accounts);
+    let transfer_trial = transfer(&mut accounts, 2, 3, 8);
+    match transfer_trial{
+        Ok(())=>println!("Success"),
+        Err(msg)=>println!("{}",msg),
+    }
+    print_state(&accounts);
+
+    
+
+
 
 }
