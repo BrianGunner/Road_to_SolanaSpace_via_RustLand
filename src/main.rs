8c58fc6dc7c95eaac2e8af707f22c102282033c7
BrianGunner <bhargav@avalialearning.com>
2026-04-30 12:22:41 +0530

    My first mini solana

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,82 @@
-#[derive(Debug)]
+
 struct Account{
     id:u32,
     balance:i32,
 }
+#[derive(Debug)]
+struct TransferPlan{
+    from_index:usize,
+    to_index:usize,
+    amount:i32,
+}
 
-fn find_index(accounts:&Vec<Account>,id:u32)->Option<usize>{
+fn print_state(accounts:&Vec<Account>){
+    println!("🤑++++++++++++++🤑");
+    for acc in accounts.iter(){
+        println!("Id: {} --- Balance: {}",acc.id,acc.balance)
+    }
+    println!("🤑++++++++++++++🤑");
+    
+}
+
+fn find_index(accounts:&[Account],id:u32)->Option<usize>{
     for (index,acc) in accounts.iter().enumerate(){
         if acc.id == id{
-            return Some(index)
+            return Some(index);
         }
     }
     None
 }
 
-fn transfer(accounts:&mut Vec<Account>,from_id:u32,to_id:u32,amount:i32)->Result<(),String>{
-    let from_id_index = find_index(accounts, from_id).ok_or("from_id not found".to_string())?;
-    let to_id_index = find_index(accounts, to_id).ok_or("to_id not found".to_string())?;
-    if from_id_index==to_id_index{
-        return Err("from id cannot be same as to id".to_string());
+fn build_transfer_plan(accounts:&Vec<Account>,from_id:u32,to_id:u32,amount:i32)->Result<TransferPlan,String>{
+    let from_index = find_index(accounts, from_id).ok_or("Could not find from id".to_string())?;
+    let to_index = find_index(accounts, to_id).ok_or("Could not find to id".to_string())?;
+    if from_index == to_index{
+        return Err("from id cannot be to id".to_string());
     }
-    if accounts[from_id_index].balance<amount{
+    if accounts[from_index].balance<amount{
         return Err("Insufficient balance".to_string());
     }
-    if from_id_index<to_id_index{
-       let (left,right) = accounts.split_at_mut(to_id_index);
-        left[from_id_index].balance-=amount;
-        right[0].balance+=amount;
-    }
-    else {
-        let(left,right)=accounts.split_at_mut(from_id_index);
-        right[0].balance-=amount;
-        left[to_id_index].balance+=amount;
-    }
-
-    Ok(())
+    Ok(TransferPlan { from_index,to_index,amount})
 }
 
-fn print_state(accounts:&Vec<Account>){
-    println!("🤑++++++++++++++++🤑");
-    for acc in accounts.iter(){
-        
-        println!("Account id: {} - Account balance: {}",acc.id,acc.balance);
-    
+fn execute_transfer(accounts:&mut Vec<Account>,plan:TransferPlan){
+    if plan.from_index<plan.to_index{
+        let (left,right )= accounts.split_at_mut(plan.to_index);
+        {
+            left[plan.from_index].balance-=plan.amount;
+            right[0].balance+=plan.amount;
+        }
+    }
+    else if plan.from_index>plan.to_index{
+        let (left,right) = accounts.split_at_mut(plan.from_index);
+        {
+            right[0].balance-=plan.amount;
+            left[plan.to_index].balance+=plan.amount;
+
     }
 }
+}
 
-
-
-
-fn main(){
+fn main()->Result<(),String>{
     let mut accounts = vec![
-        Account{id:1,balance:1000},
-        Account{id:2,balance:110},
-        Account{id:3,balance:0},
-        Account{id:4,balance:111},
-        Account{id:5,balance:98898},
+        Account{id:1,balance:9999},
+        Account{id:2,balance:0},
+        Account{id:3,balance:78},
+        Account{id:4,balance:0},
+        Account{id:5,balance:25000},
+        Account{id:6,balance:8888},
     ];
-    
+
     print_state(&accounts);
-    let transfer_trial = transfer(&mut accounts, 2, 3, 8);
-    match transfer_trial{
-        Ok(())=>println!("Success"),
-        Err(msg)=>println!("{}",msg),
-    }
+    let tx_1 = build_transfer_plan(&accounts, 2, 2, 99)?;
+    execute_transfer(&mut accounts, tx_1);
     print_state(&accounts);
+    return Ok(());
 
     
 
-
+   
+   
 
 }
