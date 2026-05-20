08daa20c41d771176a9f4beebdb3f49d07d5836c
BrianGunner <bhargav@avalialearning.com>
2026-05-20 18:21:10 +0530

    Mini Solana V2 mutlti program type  instructions

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,33 @@
 
+
 #[derive(Debug)]
 #[derive(PartialEq)]
 enum ProgramType{
     System,
     Token,
-    Other,
+    Others,
 }
+
 struct Account{
     address:u32,
-    lamports:i32,
+    lamports:u64,
+    token_balance:u64,
     owner:ProgramType,
 }
 impl Account{
-    fn create_account(accounts:&mut Vec<Account>,address:u32,lamports:i32,owner:ProgramType)->Result<(),String>{
+    fn create_new(accounts:&mut Vec<Account>,address:u32,lamports:u64,token_balance:u64,owner:ProgramType)->Result<(),String>{
         for acc in accounts.iter(){
             if acc.address==address{
                 return Err("Address already exists".to_string());
             }
         }
-        accounts.push(Account{address,lamports,owner});
+        accounts.push(Account { address, lamports, token_balance, owner });
         Ok(())
     }
     fn print_account_state(accounts:&[Account]){
+        println!("🤑+++++++++++++++++++++++🤑");
         for acc in accounts.iter(){
-            println!("Address: {},Lamports: {}, Owner: {:?}", acc.address,acc.lamports,acc.owner)
+            println!("Address: {}, Lamports: {}, Token Balance: {}, Owner: {:?}",acc.address,acc.lamports,acc.token_balance,acc.owner)
         }
     }
     fn find_index(accounts:&[Account],address:u32)->Option<usize>{
@@ -35,106 +39,127 @@ impl Account{
         None
     }
 }
-
 struct TxInstruction{
-    from_add:u32,
-    to_add:u32,
-    amount:i32,
+    from_address:u32,
+    to_address:u32,
+    amount:u64,
     program:ProgramType,
 }
-
 impl TxInstruction{
-    fn create_tx_instruction(transactions:&mut Vec<TxInstruction>,from_add:u32,to_add:u32,amount:i32,program:ProgramType){
-        transactions.push(TxInstruction { from_add, to_add, amount, program })
+    fn create_tx_inst(transactions:&mut Vec<TxInstruction>,from_address:u32,to_address:u32,amount:u64,program:ProgramType){
+        transactions.push(TxInstruction { from_address, to_address, amount, program })
     }
     fn print_tx_state(transactions:&[TxInstruction]){
+        println!("📒______________________📒");
         for txs in transactions.iter(){
-            println!("From: {},To: {},Amount: {},Program: {:?}",txs.from_add,txs.to_add,txs.amount,txs.program)
+            println!("From: {},To: {},Amount: {},Program: {:?}",txs.from_address,txs.to_address,txs.amount,txs.program)
         }
     }
-
-    fn validate_txs(accounts:&[Account],tx:&TxInstruction)->Result<Plan,String>{
-        if tx.program!=ProgramType::System{
-            return Err("Program not System".to_string())
+    fn token_validator(accounts:&[Account],instruction:&TxInstruction)->Result<Plan,String>{
+        if instruction.program!=ProgramType::Token{
+            return Err("Program not Token".to_string())
         }
-        let from_index = Account::find_index(&accounts, tx.from_add).ok_or("from not found".to_string())?;
-        let to_index = Account::find_index(&accounts, tx.to_add).ok_or("to not found".to_string())?;
+        let from_index = Account::find_index(&accounts, instruction.from_address).ok_or("from not found".to_string())?;
+        let to_index = Account::find_index(&accounts, instruction.to_address).ok_or("to not found".to_string())?;
         if from_index==to_index{
             return Err("From id cannot be same as to id".to_string())
         }
-        if accounts[from_index].lamports<tx.amount{
+        if accounts[from_index].token_balance<instruction.amount{
             return Err("Insufficient Balance".to_string());
         }
-        if tx.amount<=0{
-            return Err("Transfer amount should be more than zero".to_string());
+        if accounts[from_index].owner!=ProgramType::Token{
+            return Err("From Account owner not System".to_string());
         }
+        if accounts[to_index].owner!=ProgramType::Token{
+            return Err("To id owner not System".to_string());
+        }
+        Ok(Plan { from_index, to_index, amount: instruction.amount,program:ProgramType::Token})
+    }
 
+    fn system_validator(accounts:&[Account],instruction:&TxInstruction)->Result<Plan,String>{
+        if instruction.program!=ProgramType::System{
+            return Err("Program not System".to_string())
+        }
+        let from_index = Account::find_index(&accounts, instruction.from_address).ok_or("from not found".to_string())?;
+        let to_index = Account::find_index(&accounts, instruction.to_address).ok_or("to not found".to_string())?;
+        if from_index==to_index{
+            return Err("From id cannot be same as to id".to_string())
+        }
+        if accounts[from_index].lamports<instruction.amount{
+            return Err("Insufficient Balance".to_string());
+        }
         if accounts[from_index].owner!=ProgramType::System{
             return Err("From Account owner not System".to_string());
         }
         if accounts[to_index].owner!=ProgramType::System{
             return Err("To id owner not System".to_string());
         }
-        Ok(Plan { from_index, to_index, amount: tx.amount })
+        Ok(Plan { from_index, to_index, amount: instruction.amount,program:ProgramType::System})
     }
 
     }
-
+#[derive(Debug)]
 struct Plan{
     from_index:usize,
     to_index:usize,
-    amount:i32,
+    amount:u64,
+    program:ProgramType,
 }
 
 impl Plan{
-    fn execute_plan(accounts:&mut [Account],plan_e:&Plan){
+    fn execute_system_plan(accounts:&mut[Account],plan_e:&Plan){
         accounts[plan_e.from_index].lamports-=plan_e.amount;
         accounts[plan_e.to_index].lamports+=plan_e.amount;
     }
-    fn print_plans(plans:&[Plan]){
-        for plan in plans.iter(){
-            println!("From index: {},To index: {},Amount: {}",plan.from_index,plan.to_index,plan.amount)
-        }
+    fn execute_token_plan(accounts:&mut[Account],plan_e:&Plan){
+        accounts[plan_e.from_index].token_balance-=plan_e.amount;
+        accounts[plan_e.to_index].token_balance+=plan_e.amount;
     }
 }
 
 struct RunTime;
 impl RunTime{
-    fn process_instructions(accounts:&mut [Account],transactions:&[TxInstruction])->Result<(),String>{
-       let mut plans = Vec::new();
-       for txs in transactions.iter(){
-            let tx_1 = TxInstruction::validate_txs(accounts, txs);
-            match tx_1{
-                Ok(value)=>{
-                    plans.push(value);
-                },
-                Err(msg)=>{
-                    return Err(msg);
-                }
-            }
-            
+    fn Runtime(accounts:&mut[Account],transactions:&[TxInstruction])->Result<(),String>{
+        let mut plans = Vec::new();
+        let ins_sys = &transactions[0];
+        let ins_token = &transactions[1];
+        match TxInstruction::system_validator(accounts, &ins_sys){
+            Ok(value)=>plans.push(value),
+            Err(msg)=>return Err(msg),
         }
-        for plan_ in plans.iter(){
-            Plan::execute_plan(accounts, plan_);
+        match TxInstruction::token_validator(accounts, &ins_token){
+            Ok(value)=>plans.push(value),
+            Err(msg)=>return Err(msg),
+        }
+
+        for plan in plans.iter(){
+            if plan.program==ProgramType::System{
+                Plan::execute_system_plan(accounts, plan);
+            }
+            if plan.program==ProgramType::Token{
+                Plan::execute_token_plan(accounts, plan);
+            }   
         }
         Ok(())
     }
 }
+
 fn main(){
     let mut accounts: Vec<Account> = Vec::new();
     let mut transactions: Vec<TxInstruction> = Vec::new();
-    Account::create_account(&mut accounts, 1, 99, ProgramType::System);
-    Account::create_account(&mut accounts, 2, 1099, ProgramType::System);
-    Account::create_account(&mut accounts, 3, 99, ProgramType::System);
-    Account::create_account(&mut accounts, 4, 0, ProgramType::System);
+    Account::create_new(&mut accounts, 1, 1000, 0, ProgramType::System);
+    Account::create_new(&mut accounts, 2, 0, 0, ProgramType::Token);
+    Account::create_new(&mut accounts, 3, 0, 0, ProgramType::System);
+    Account::create_new(&mut accounts, 4, 0, 10000, ProgramType::Token);
     Account::print_account_state(&accounts);
-    TxInstruction::create_tx_instruction(&mut transactions, 1, 4, 55, ProgramType::System);
-    TxInstruction::create_tx_instruction(&mut transactions, 2, 4, 1000, ProgramType::System);
-    TxInstruction::create_tx_instruction(&mut transactions, 3, 4, 99, ProgramType::System);
-    match RunTime::process_instructions(&mut accounts, &transactions){
-        Ok(_)=>println!("Transactions successfully submitted"),
+    TxInstruction::create_tx_inst(&mut transactions, 1, 3, 500, ProgramType::System);
+    TxInstruction::create_tx_inst(&mut transactions, 4, 2, 5000, ProgramType::Token);
+    TxInstruction::print_tx_state(&transactions);
+    let runtime_1 = RunTime::Runtime(&mut accounts, &transactions);
+    match runtime_1{
+        Ok(value)=>println!("Validation successful and execution done"),
         Err(msg)=>println!("{}",msg),
     }
     Account::print_account_state(&accounts);
-    
+
 }
