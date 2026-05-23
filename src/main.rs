9b0b73623b19cde7c988cf2f4e1bc634a20a44f9
BrianGunner <bhargav@avalialearning.com>
2026-05-23 16:17:12 +0530

    Data learning 2

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,32 @@
-fn main(){
-    let number: u64 = 123456;
-    println!("Number {}",number);
-    let data:Vec<u8> = number.to_le_bytes().to_vec();
-    println!("Store Data is : {:?}",data);
 
-    let mut bytes = [0u8;8];
-    bytes.copy_from_slice(&data[0..8]);
-    let recovered = u64::from_le_bytes(bytes);
-    println!("{:?}",recovered);
+
+fn main(){
+   let mut data:Vec<u8> = Vec::new();
+   let num1= 1001_u64;
+   let num1_as_bytes = num1.to_le_bytes();
+   for byte in num1_as_bytes{
+        data.push(byte);
+   }
+   println!("{:?}",data);
+   let num2: u64 = 5000;
+   let num2_as_bytes = num2.to_le_bytes();
+   for bytes in num2_as_bytes{
+    data.push(bytes);
+   }
+   println!("{:?}",data);
+
+   let mut bytes_1 = [0u8;8];
+   bytes_1.copy_from_slice(&data[0..8]);
+   let mut bytes_2 = [0u8;8];
+   bytes_2.copy_from_slice(&data[8..16]);
+   let recover_1 = u64::from_le_bytes(bytes_1);
+   let recover_2 = u64::from_le_bytes(bytes_2);
+   println!("Number 1 after conversion and reconversion: {}",recover_1);
+   println!("Number 2 after conversion and reconversion: {}",recover_2);
 }
+
+
+
+
+
+
