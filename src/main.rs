c993d8a2eb75396aece63f24549a1d5631e8ce22
BrianGunner <bhargav@avalialearning.com>
2026-05-23 15:11:30 +0530

    basic of data

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 fn main(){
-    let number:u64 = 12345;
-    let data:Vec<u8> = number.to_ne_bytes().to_vec();
-    println!("Number stored as data le : {:?}",data);
+    let number: u64 = 123456;
+    println!("Number {}",number);
+    let data:Vec<u8> = number.to_le_bytes().to_vec();
+    println!("Store Data is : {:?}",data);
 
-    let mut bytes = [0u8; 8];
+    let mut bytes = [0u8;8];
     bytes.copy_from_slice(&data[0..8]);
     let recovered = u64::from_le_bytes(bytes);
     println!("{:?}",recovered);
-     
 }
